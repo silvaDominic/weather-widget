48e65b56f8710da36de580c2eafcd094aca3c4eb
Dominic <string.reverse@proton.me>
2024-09-16 19:00:49 -0400

    Updated geolocation interface

--- src/application/models/geolocation-service.interface.ts
+++ src/application/models/geolocation-service.interface.ts
@@ -2,5 +2,6 @@ import { IGeolocationResponse } from './geo-response.interface';
 
 export interface IGeolocationService {
   getCurrentLocationCoords(): Promise<IGeolocationResponse>,
-  getGeolocationByZipcode(zipcode: string): Promise<IGeolocationResponse>
+  getGeolocationByZipcode(zipcode: string): Promise<IGeolocationResponse>,
+  getGeolocationByCity(city: string): Promise<IGeolocationResponse>,
 }
