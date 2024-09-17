b0e1a48d80153d35f4190e4180cc9b21dc250a5f
Dominic <string.reverse@proton.me>
2024-09-16 21:48:54 -0400

    Merged other weather domain models into single DailyWeatherModel

--- src/application/models/daily-weather.model.ts
+++ src/application/models/daily-weather.model.ts
@@ -1,19 +1,23 @@
 export class DailyWeatherModel {
+    location: string;
     date: number;
     description: string;
     temp: number;
-    humidity: number;
     maxTemp: number;
     minTemp: number;
     windSpeed: number;
+    windDirection: string;
+    humidity: number;
 
     constructor() {
+        this.location = "";
         this.date = 0;
         this.description = "";
         this.temp = 0;
-        this.humidity = 0;
         this.maxTemp = 0;
         this.minTemp = 0;
         this.windSpeed = 0;
+        this.windDirection = "";
+        this.humidity = 0;
     }
 }
