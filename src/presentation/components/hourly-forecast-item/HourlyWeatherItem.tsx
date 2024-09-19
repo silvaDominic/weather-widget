f8da2e2053f75f426c5b02224351bc90671972cd
Dominic <string.reverse@proton.me>
2024-09-19 15:45:15 -0400

    Refactor structure/styling
    - Remove old styling rules in app
    - Restructured HTML to accommodate boostrap layout

--- src/presentation/components/hourly-forecast-item/HourlyWeatherItem.tsx
+++ src/presentation/components/hourly-forecast-item/HourlyWeatherItem.tsx
@@ -5,15 +5,16 @@ import './HourlyForecastItem.css'
 export type HourlyWeatherItemProps = {
   hour: number,
   temp: number,
+  clickHandler: () => void;
 }
 
-export function HourlyWeatherItem({hour, temp}: HourlyWeatherItemProps) {
+export function HourlyWeatherItem({hour, temp, clickHandler}: HourlyWeatherItemProps) {
   return (
-    <div className="hourly-list-item flex-grow">
+    <li className="list-group-item" onClick={clickHandler}>
       <div className="hourly-list-item-wrapper">
         <h3 className="hour">{unixToHour(hour)}</h3>
         <p className="temp">{temp}F</p>
       </div>
-    </div>
+    </li>
   )
 }
--- src/presentation/components/weather-widget/WeatherWidget.tsx
+++ src/presentation/components/weather-widget/WeatherWidget.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, Fragment, useState } from 'react';
+import React, { FormEvent, Fragment, useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import './WeatherWidget.css';
 // Helpers
@@ -10,9 +10,7 @@ import { useWeather } from "../../hooks/use-weather.hook";
 
 export function WeatherWidget() {
   // Comp state
-  const [activeDay, setActiveDay] = useState("");
-  const [location, setLocation] = useState("");
-  const [day, setDay] = useState("");
+  const [selectedWeather, setSelectedWeather] = useState<DailyWeatherModel>(new DailyWeatherModel());
   const [isSearching, setIsSearching] = useState(false);
   const {
     currentWeather,
@@ -20,6 +18,12 @@ export function WeatherWidget() {
     hourlyWeather,
     getWeatherByCityOrZipcode
   } = useWeather();
+
+  useEffect(() => {
+    if (currentWeather) {
+      setSelectedWeather(currentWeather);
+    }
+  }, []);
 
   function getRoundedTemp(temp: number): number {
     return Math.round(temp);
@@ -37,69 +41,87 @@ export function WeatherWidget() {
     return Math.round(temp);
   }
 
+  function onHourlyWeatherClick(hourlyWeather: DailyWeatherModel) {
+    setSelectedWeather(hourlyWeather);
+  }
+
   return (
-    <div id="weather-widget-container" className="container">
-      <div id="header-container" className="flex-space-between">
-        <div id="todays-weather-brief-container" className="">
-          <h1>Now</h1>
-          <h1>{getRoundedTemp(currentWeather.temp)}F</h1>
-          <div>Wind: {getRoundedWindSpeed(currentWeather.windSpeed)} mph</div>
-          <div>Humidity: {currentWeather.humidity}%</div>
-        </div>
-
-        <div className="text-center">
-          <h1>{currentWeather.location}</h1>
-          <h2>{unixToDay(currentWeather.date, true)}</h2>
-        </div>
-
-        <div id="form-container">
-          <form id="location-form" onSubmit={searchLocation}>
-            <div className="form-control">
-              <input placeholder="Search by city or zipcode" required id="zipcode-text-field" name="location" type="text"/>
+    <div className='container'>
+      <div id="form-container" className='my-3'>
+        <form id="location-form" onSubmit={searchLocation}>
+          <div className="row">
+            <div className="col-11">
+              <input
+                className='form-control'
+                placeholder="Search by city or zipcode"
+                required id="zipcode-text-field"
+                name="location"
+                type="text"/>
             </div>
 
-            <button disabled={isSearching}>Search</button>
-          </form>
+            <div className="col-1">
+              <button className='btn btn-primary' disabled={isSearching}>Search</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div id="current-weather-container" className='my-3'>
+        <div className="row">
+          <div id="header-container" className="col-12">
+            <div className="text-center mb-3">
+              <span className='h3'>{currentWeather.location}</span>,
+              <span className='h3'>{unixToDay(currentWeather.date, true)}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="row align-items-center my-3">
+          <div id='weather-icon' className='col-3'>
+            <img src="" alt=""/>
+          </div>
+
+          <div id='current-weather-brief' className='col-3'>
+            <h3>Now</h3>
+            <div>{getRoundedTemp(currentWeather.temp)}F</div>
+            <div>Wind: {getRoundedWindSpeed(currentWeather.windSpeed)} mph</div>
+            <div>Humidity: {currentWeather.humidity}%</div>
+          </div>
+
+          <div id="daily-weather-detail-container" className='col-6'>
+            <h3>{capitalize(selectedWeather.description)}</h3>
+            <p>The high will be {getRoundedHighTemp(selectedWeather.maxTemp)}F, the low will
+              be {getRoundedLowTemp(selectedWeather.minTemp)}F.</p>
+
+            <div>Wind: {getRoundedWindSpeed(selectedWeather.windSpeed)} mph</div>
+            <div>Humidity: {selectedWeather.humidity}%</div>
+          </div>
         </div>
       </div>
 
-      <div id="hourly-weather-container">
+      <div id="hourly-weather-container" className='row'>
         <h2 className='text-center'>24 Hour Forecast</h2>
-        <div>
-          {
-            (hourlyWeather.length !== 0)
-              ? hourlyWeather.map((hourlyWeather: DailyWeatherModel) => (
-                <Fragment key={hourlyWeather.date}>
+        <div className="col-12 my-3">
+          <ul className='list-group list-group-horizontal'>
+            {
+              (hourlyWeather.length !== 0)
+                ? hourlyWeather.map((hourlyWeather: DailyWeatherModel) => (
+                  <Fragment
+                    key={hourlyWeather.date}>
                   <HourlyWeatherItem
-                    hour={hourlyWeather.date}
-                    temp={hourlyWeather.temp}/>
-                </Fragment>
-              ))
-              : <div>No hourly data available.</div>
-          }
+                      hour={hourlyWeather.date}
+                      temp={hourlyWeather.temp}
+                      clickHandler={() => setCurrentWeather(hourlyWeather)}
+                    />
+                  </Fragment>
+                ))
+                : <div>No hourly data available.</div>
+            }
+          </ul>
         </div>
       </div>
-
-      <div id="daily-weather-detail-container">
-        <div>
-          <h3>{capitalize(currentWeather.description)}</h3>
-          <p>The high will be {getRoundedHighTemp(currentWeather.maxTemp)}F, the low will
-            be {getRoundedLowTemp(currentWeather.minTemp)}F.</p>
-        </div>
-
-        <div>Wind: {getRoundedWindSpeed(currentWeather.windSpeed)} mph</div>
-        <div>Humidity: {currentWeather.humidity}%</div>
-      </div>
-
     </div>
   );
-
-  // function setDailyWeather(day: string): void {
-  //   setActiveDay(day);
-  //   // Get DailyForecast object based on day and set TodaysWeatherDetail with it
-  //   const dailyForecast = weeklyWeather.find((weather: DailyForecastVM) => weather.getDay() === day);
-  //   setTodaysWeatherDetail(dailyForecast || new DailyForecastVM());
-  // }
 
   /**
    * Searches location based on data acquired from search form
