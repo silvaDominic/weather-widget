bc200a3dfa6f4a31f597748a88deba9a5d2b9e22
Dominic <string.reverse@proton.me>
2024-09-19 15:43:55 -0400

    Added bootstrap 5

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import './normalize.css';
 import App from './presentation/App';
