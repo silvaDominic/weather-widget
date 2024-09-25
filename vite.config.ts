68f2cbf4bb62199ba547b2487b00d8dea2149fa0
Dominic <string.reverse@proton.me>
2024-09-25 00:56:47 -0400

    Reverted to using process.env to get .env vars
    - Also adjusted ts.config to add the 'shared' directory to the paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  root: 'src/presentation',
-  build: {
-    outDir: '../../dist/presentation'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),    },
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    root: path.resolve(__dirname, 'src/presentation'), // Set root to presentation
+    build: {
+      outDir: path.resolve(__dirname, 'dist/presentation'), // Output directory
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    define: {
+      'process.env': loadEnv(mode, process.cwd()), // Load environment variables from the root .env file
+    },
+  };
 });
